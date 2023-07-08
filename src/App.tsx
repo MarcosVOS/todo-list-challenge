f1d73aebefe89f3fc238599c311bd0eff6f639b9
MarcosVOS <Marcos.OliveiraDEV@gmail.com>
2023-07-08 00:30:36 -0300

    Cria campo de seção tarefas criadas e tarefas concluidas

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,15 @@ function App() {
       <h1 className={styles.logoTo}>to</h1>
       <h1 className={styles.logoDo}>do</h1>
      </header>
-     <main >
+     <main>
       <section className={styles.section}>
         <Input/>
         <Button/>
       </section>
+      <div className={styles.taskStatus}>
+        <h6 className={styles.tasksCreated}>Tarefas criadas<span className={styles.taskCount}>0</span></h6>
+        <h6 className={styles.tasksDone}>Concluídas<span className={styles.taskCount}>0</span></h6>
+      </div>
      </main>
     </>
   )
