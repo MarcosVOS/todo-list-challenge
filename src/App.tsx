fe7225faf342ffdd7e0e448eadf3d1cd32ff984b
MarcosVOS <Marcos.OliveiraDEV@gmail.com>
2023-07-08 20:56:02 -0300

    Finaliza a criação de toda a parte de estilização

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import styles from './App.module.css'
 import './global.css'
 import { Input } from "./components/Input"
 import { Button } from "./components/Button"
+import ClipboardIcon from "../src/components/Icons/Clipboard.svg";
+import { Task } from "./components/Task"
+
 
 function App() {
+
+  let thereAreTasks = true 
   return (
     <>
      <header className={styles.header}> 
@@ -21,6 +26,14 @@ function App() {
         <h6 className={styles.tasksCreated}>Tarefas criadas<span className={styles.taskCount}>0</span></h6>
         <h6 className={styles.tasksDone}>Concluídas<span className={styles.taskCount}>0</span></h6>
       </div>
+      {thereAreTasks ?<div className={styles.taskContainer}><Task/></div> : 
+      <div className={styles.thereIsNoTask}>
+        <img src={ClipboardIcon} alt="" />
+        <span className={styles.thereIsNoTaskMessage}>
+          <strong>Você ainda não tem tarefas cadastradas</strong>
+          Crie tarefas e organize seus itens a fazer
+        </span>
+      </div>}
      </main>
     </>
   )
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,3 +1,13 @@
+import { TrashIcon } from "./Icons/TrashIcon";
+import { UncheckedIcon } from "./Icons/UncheckedIcon";
+
+import styles from './Task.module.css'
+
 export function Task(){
-    return(<>Task</>)
+    return(
+    <div className={styles.taskContainer}>
+        <UncheckedIcon/>
+        <span className={styles.taskText}>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</span>
+        <TrashIcon/>
+    </div>)
 }
